52ffc59669b58c3bcc1f941671ae8f34938e5082
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-10-20 17:52:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ button.addEventListener("click", f);
 function f(){
 	let p = document.getElementById("status");
 	// p.tagName = "h1";
-	// p.innerText = "Entered Metaverse";
-	p.remove();
+	p.innerText = "";
+	// p.remove();
 	let h1 = document.createElement("h1");
 	h1.id = "status";
 	h1.innerText = "Entered Metaverse";
-	document.body.insertBefore(h1, button);
+	p.append(h1);
 }
