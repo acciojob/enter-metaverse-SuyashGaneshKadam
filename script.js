e860857799da1a233e4ea884dbca8b0fa92a00f9
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-10-20 17:32:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function f(){
+	let p = document.getElementById("status");
+	let button = document.getElementById("enterBtn");
+	p.remove();
+	let h1 = document.createElement("h1");
+	h1.innerText = "Entered Metaverse";
+	document.body.insertBefore(h1, button);
+}
