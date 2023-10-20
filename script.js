5f4b659be572ff63c07bd41ce509b612168efbe5
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-10-20 17:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //your JS code here. If required.
+let button = document.getElementById("enterBtn");
+button.addEventListener("click", f);
 function f(){
 	let p = document.getElementById("status");
-	let button = document.getElementById("enterBtn");
-	p.remove();
-	let h1 = document.createElement("h1");
-	h1.innerText = "Entered Metaverse";
-	document.body.insertBefore(h1, button);
+	p.tagName = "h1";
+	p.innerText = "Entered Metaverse";
+	// p.remove();
+	// let h1 = document.createElement("h1");
+	// h1.innerText = "Entered Metaverse";
+	// document.body.insertBefore(h1, button);
 }
